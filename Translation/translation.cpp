30d3c1fc2fd07b57b6fd696021c8c5376ff24eb8
Juan Ignacio Suarez <juanigsrz@gmail.com>
2019-11-07 11:11:45 -0300

    Completed translation general structure

--- Translation/translation.cpp
+++ Translation/translation.cpp
@@ -1,1 +1,103 @@
 #include "translation.h"
+
+using namespace trans;
+
+/**
+ * Main translating class
+ * **/
+std::unique_ptr<TranslatedExp> simpleVar() {
+    // TODO: Complete
+    return nullptr;
+}
+std::unique_ptr<TranslatedExp> fieldVar() {
+    // TODO: Complete
+    return nullptr;
+}
+std::unique_ptr<TranslatedExp> subscriptVar() {
+    // TODO: Complete
+    return nullptr;
+}
+
+/**
+ * Expression translation
+ * **/
+std::unique_ptr<TranslatedExp> varExp() {
+    // TODO: Complete
+    return nullptr;
+}
+std::unique_ptr<TranslatedExp> unitExp() {
+    // TODO: Complete
+    return nullptr;
+}
+std::unique_ptr<TranslatedExp> nilExp() {
+    // TODO: Complete
+    return nullptr;
+}
+std::unique_ptr<TranslatedExp> intExp() {
+    // TODO: Complete
+    return nullptr;
+}
+std::unique_ptr<TranslatedExp> stringExp() {
+    // TODO: Complete
+    return nullptr;
+}
+std::unique_ptr<TranslatedExp> callExp() {
+    // TODO: Complete
+    return nullptr;
+}
+std::unique_ptr<TranslatedExp> opExp() {
+    // TODO: Complete
+    return nullptr;
+}
+std::unique_ptr<TranslatedExp> recordExp() {
+    // TODO: Complete
+    return nullptr;
+}
+std::unique_ptr<TranslatedExp> seqExp() {
+    // TODO: Complete
+    return nullptr;
+}
+std::unique_ptr<TranslatedExp> assignExp() {
+    // TODO: Complete
+    return nullptr;
+}
+std::unique_ptr<TranslatedExp> ifExp() {
+    // TODO: Complete
+    return nullptr;
+}
+std::unique_ptr<TranslatedExp> whileExp() {
+    // TODO: Complete
+    return nullptr;
+}
+std::unique_ptr<TranslatedExp> forExp() {
+    // TODO: Complete
+    return nullptr;
+}
+std::unique_ptr<TranslatedExp> letExp() {
+    // TODO: Complete
+    return nullptr;
+}
+std::unique_ptr<TranslatedExp> breakExp() {
+    // TODO: Complete
+    return nullptr;
+}
+std::unique_ptr<TranslatedExp> arrayExp() {
+    // TODO: Complete
+    return nullptr;
+}
+
+/**
+ * Declaration translation
+ * **/
+std::unique_ptr<TranslatedExp> varDec() {
+    // TODO: Complete
+    return nullptr;
+}
+std::unique_ptr<TranslatedExp> typeDec() {
+    // TODO: Complete
+    return nullptr;
+}
+std::unique_ptr<TranslatedExp> funDec() {
+    // TODO: Complete
+    return nullptr;
+}
--- Translation/translation.h
+++ Translation/translation.h
@@ -7,10 +7,9 @@
  * Described in Chapter 7 Appel C (2004)
  * **/
 
-
+#include "translatedExpression.h"
 
 namespace trans {
-
 
 
 /**
@@ -19,7 +18,39 @@ namespace trans {
 class Translator {
     // Stuff
 public:
-    // More stuff
+    /**
+     * Variable translation
+     * **/
+    std::unique_ptr<TranslatedExp> simpleVar();
+    std::unique_ptr<TranslatedExp> fieldVar();
+    std::unique_ptr<TranslatedExp> subscriptVar();
+    
+    /**
+     * Expression translation
+     * **/
+    std::unique_ptr<TranslatedExp> varExp();
+    std::unique_ptr<TranslatedExp> unitExp();
+    std::unique_ptr<TranslatedExp> nilExp();
+    std::unique_ptr<TranslatedExp> intExp();
+    std::unique_ptr<TranslatedExp> stringExp();
+    std::unique_ptr<TranslatedExp> callExp();
+    std::unique_ptr<TranslatedExp> opExp();
+    std::unique_ptr<TranslatedExp> recordExp();
+    std::unique_ptr<TranslatedExp> seqExp();
+    std::unique_ptr<TranslatedExp> assignExp();
+    std::unique_ptr<TranslatedExp> ifExp();
+    std::unique_ptr<TranslatedExp> whileExp();
+    std::unique_ptr<TranslatedExp> forExp();
+    std::unique_ptr<TranslatedExp> letExp();
+    std::unique_ptr<TranslatedExp> breakExp();
+    std::unique_ptr<TranslatedExp> arrayExp();
+    
+    /**
+     * Declaration translation
+     * **/
+    std::unique_ptr<TranslatedExp> varDec();
+    std::unique_ptr<TranslatedExp> typeDec();
+    std::unique_ptr<TranslatedExp> funDec();
 };
 
 
