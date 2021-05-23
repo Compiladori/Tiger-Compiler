790a73446f41ca353d7a1b72cbe99a8c5c5c1f56
lisandro <lisandromaselli@gmail.com>
2021-05-22 21:11:53 -0300

    added initArray

--- dep/runtime.c
+++ dep/runtime.c
@@ -26,6 +26,12 @@ void printi(int k) {
     printf("%d", k);
 }
 
+int *initArray(int size, int init) {
+    int i;
+    int *a = (int *)malloc(size * sizeof(int));
+    for ( i = 0; i < size; i++ ) a[i] = init;
+    return a;
+}
 int main() {
     printf("\n");
     return tigermain(0 /* static link */);
