050cb4038dd309d3a1ed0235e8ebb3619bfad72d
OriReschini <orireschini@gmail.com>
2021-05-30 23:18:09 -0300

    Tests compile (some cases fail though).

--- Testing/testMunch.cpp
+++ Testing/testMunch.cpp
@@ -13,14 +13,14 @@ TEST_CASE("print", "[assem]") {
     SECTION("Oper") {
         stringstream out;
         auto oper = make_unique<assem::Oper>("jmp 'j0", temp::TempList{}, temp::TempList{}, temp::LabelList(1, lbl));
-        oper->print(out, map);
+        oper->print();
         auto str = out.str();
         REQUIRE(str.compare("jmp L48") == 0);
     }
     SECTION("Label") {
         stringstream out;
         auto oper = make_unique<assem::Label>(lbl.name + ":", lbl);
-        oper->print(out, map);
+        oper->print();
         auto str = out.str();
         REQUIRE(str.compare("L50:") == 0);
     }
@@ -28,7 +28,7 @@ TEST_CASE("print", "[assem]") {
     SECTION("Move") {
         stringstream out;
         auto oper = make_unique<assem::Move>("movq %'s0, %'d0", temp::TempList(1, tmp), temp::TempList(1, tmp));
-        oper->print(out, map);
+        oper->print();
         auto str = out.str();
         REQUIRE(str.compare("movq %L52, %L52") == 0);
     }
